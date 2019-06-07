089b4f6942390289b7a85820e8e2f436d8df4044
Fatih Sağlam <m.fatihsaglam@outlook.com>
2019-06-08 00:09:38 +0300

    Add project files.

---
+++ PaparaMVCExample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PaparaMVCExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
